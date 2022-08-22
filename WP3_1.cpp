f083e88062233cf2029331ca6147b9e565d14b1a
Tup <65010377@kmitl.ac.th>
2022-08-22 15:29:57 +0700

    WP3

---
+++ WP3_1.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e,sum;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a < b &&a < c)
+    {
+        d = a;
+    }
+        else if(b < a &&b < c)
+        {
+            d = b;
+        }
+        else if(c < a &&c < b)
+        {
+            d = c;
+        }
+        else if((a > b &&a < c) ||(a > c &&a < b))
+        {
+            e = a;
+        }
+        else if((b > a &&b < c) ||(b > c &&b < a))
+        {
+            e = b;
+        }
+        else if((c > a &&c < b)||(c > b &&c < a))
+        {
+            e = c;
+        }
+    sum = d + e;
+    printf("%d",sum);
+    return 0;
+}
