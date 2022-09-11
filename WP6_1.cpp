839f6d9d583f845ca752eb7974abf39f4f432b9c
Tup <65010377@kmitl.ac.th>
2022-09-11 23:06:15 +0700

    WP6

---
+++ WP6_1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    int i;
+    scanf("%s",str);
+    for(i = strlen(str) - 1;i >= 0;i--)
+    {
+        printf("%c",str[i]);
+    }
+    return 0;
+}
